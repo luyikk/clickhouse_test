549c475f7895429ec5eca2af75747b818fc1d411
luyi <luyikk@126.com>
2023-02-08 14:46:54 +0800

    push

--- src/main.rs
+++ src/main.rs
@@ -9,39 +9,24 @@ use tokio::time::{sleep, Instant};
 
 #[derive(Debug, Row, Serialize, Deserialize, Default)]
 pub struct LogMoneyGameSpin {
-    pub time: i64,
+    pub timestamp: i64,
     pub account_id: i32,
-    pub diff_money: f64,
-    pub diff_money_gift: f64,
     pub log_game_in_id: i64,
     pub game_id: i32,
     pub service_id: i32,
+
+    pub spin_type: i32,
+    pub is_win:bool,
     pub bet_money: f64,
     pub win_money: f64,
-    pub lock_win_money: f64,
-    pub r#type: i32,
-    pub money: f64,
-    pub money_safe: f64,
-    pub money_gift: f64,
-    pub money_gift_safe: f64,
-    pub amount_of_gift: f64,
-    #[serde(rename = "amount_of_washcode")]
-    pub amount_of_wash_code: f64,
+
     pub money_type: i32,
-    #[serde(rename = "levelup_money")]
+    pub money: f64,
+    pub money_gift: f64,
+    pub amount_of_gift: f64,
+    pub amount_of_wash_code: f64,
     pub level_up_money: i64,
-    #[serde(rename = "consum_money")]
-    pub consume_money: f64,
-    #[serde(rename = "consum_money_gift")]
-    pub consume_money_gift: f64,
-    #[serde(rename = "amount_of_slotswash")]
-    pub amount_of_slots_wash: f64,
-    #[serde(rename = "consum_money_gift_of_slots")]
-    pub consume_money_gift_of_slots: f64,
-    #[serde(rename = "consum_money_of_slots")]
-    pub consume_money_of_slots: f64,
-    pub income_money: f64,
-    pub income_money_of_slots: f64,
+    pub amount_of_slots_wash_code: f64,
 }
 
 static TPS: AtomicU64 = AtomicU64::new(0);
@@ -55,18 +40,10 @@ async fn main() -> Result<()> {
             .with_database("test")
             .with_url("http://192.168.1.221:8123"),
     );
-    //let mut table = client.insert("log_money_game_spin")?;
 
-    // table.write(&LogMoneyGameSpin{
-    //     time:chrono::prelude::Utc::now().timestamp_millis(),
-    //     account_id:1,
-    //     amount_of_wash_code:100f64,
-    //     ..Default::default()
-    // }).await?;
-    // table.end().await?;
 
     client
-        .query("ALTER TABLE log_money_game_spin DELETE WHERE `time`!=0")
+        .query("optimize table log_money_slot final")
         .execute()
         .await?;
 
@@ -77,7 +54,7 @@ async fn main() -> Result<()> {
 
             let start = Instant::now();
             if let Ok(count)=cc
-                .query("select count(*) from log_money_game_spin")
+                .query("select count(*) from log_money_slot")
                 .fetch_one::<i64>()
                 .await {
                 println!("每秒写入速度:{tps} 当前表数据量:{count}");
@@ -88,7 +65,7 @@ async fn main() -> Result<()> {
     });
 
     let mut inserter = client
-        .inserter("log_money_game_spin")?
+        .inserter("log_money_slot")?
         .with_max_entries(5000)
         .with_period(Some(Duration::from_secs(10)));
 
@@ -96,32 +73,22 @@ async fn main() -> Result<()> {
 
     let start = Instant::now();
     let x = LogMoneyGameSpin {
-        time: chrono::prelude::Utc::now().timestamp_nanos(),
+        timestamp: chrono::prelude::Utc::now().timestamp_nanos(),
         account_id: 1,
-        diff_money: rng.gen(),
-        diff_money_gift: rng.gen(),
         log_game_in_id: rng.gen_range(0..1000),
         game_id: rng.gen_range(0..100),
         service_id: 101,
+        spin_type: 1,
+        is_win: true,
         bet_money: rng.gen(),
         win_money: rng.gen(),
-        lock_win_money: rng.gen(),
-        r#type: 1,
         money: rng.gen(),
-        money_safe: rng.gen(),
         money_gift: rng.gen(),
-        money_gift_safe: rng.gen(),
         amount_of_gift: rng.gen(),
         amount_of_wash_code: rng.gen(),
         money_type: 1,
         level_up_money: rng.gen(),
-        consume_money: rng.gen(),
-        consume_money_gift: rng.gen(),
-        amount_of_slots_wash: rng.gen(),
-        consume_money_gift_of_slots: rng.gen(),
-        consume_money_of_slots: rng.gen(),
-        income_money: rng.gen(),
-        income_money_of_slots: rng.gen(),
+        amount_of_slots_wash_code: rng.gen(),
     };
     println!(
         "LogMoneyGameSpin create {} sec",
@@ -132,52 +99,36 @@ async fn main() -> Result<()> {
     for _ in 0..1000000000 {
         if let Err(err) = inserter
             .write(&LogMoneyGameSpin {
-                time: chrono::prelude::Utc::now().timestamp_nanos(),
+                timestamp: chrono::prelude::Utc::now().timestamp_nanos(),
                 account_id: 1,
-                diff_money: rng.gen(),
-                diff_money_gift: rng.gen(),
                 log_game_in_id: rng.gen_range(0..1000),
                 game_id: rng.gen_range(0..100),
                 service_id: 101,
+                spin_type: 1,
+                is_win: true,
                 bet_money: rng.gen(),
                 win_money: rng.gen(),
-                lock_win_money: rng.gen(),
-                r#type: 1,
                 money: rng.gen(),
-                money_safe: rng.gen(),
                 money_gift: rng.gen(),
-                money_gift_safe: rng.gen(),
                 amount_of_gift: rng.gen(),
                 amount_of_wash_code: rng.gen(),
                 money_type: 1,
                 level_up_money: rng.gen(),
-                consume_money: rng.gen(),
-                consume_money_gift: rng.gen(),
-                amount_of_slots_wash: rng.gen(),
-                consume_money_gift_of_slots: rng.gen(),
-                consume_money_of_slots: rng.gen(),
-                income_money: rng.gen(),
-                income_money_of_slots: rng.gen(),
+                amount_of_slots_wash_code: rng.gen(),
             })
             .await
         {
             println!("write:{}", err.to_string());
             inserter = client
-                .inserter("log_money_game_spin")?
+                .inserter("log_money_slot")?
                 .with_max_entries(5000)
                 .with_period(Some(Duration::from_secs(10)));
         }
 
-        // inserter.write(&LogMoneyGameSpin{
-        //     time:chrono::prelude::Utc::now().timestamp_millis(),
-        //     account_id:1,
-        //     amount_of_wash_code:100f64,
-        //     ..Default::default()
-        // }).await?;
         if let Err(err) = inserter.commit().await {
             println!("{}", err.to_string());
             inserter = client
-                .inserter("log_money_game_spin")?
+                .inserter("log_money_slot")?
                 .with_max_entries(5000)
                 .with_period(Some(Duration::from_secs(10)));
         } else {
@@ -188,7 +139,7 @@ async fn main() -> Result<()> {
     inserter.end().await?;
 
     client
-        .query("ALTER TABLE log_money_game_spin DELETE WHERE `time`!=0")
+        .query("optimize table log_money_slot final")
         .execute()
         .await?;
 
